79d29cabbb86a8099d55af29af53e11667dd3b2a
Jayson Garcia <garciajayson502@gmail.com>
2023-10-18 23:50:40 +0800

    chore: rename command & added result without type template [WP-200]

--- src/Commands/ResultsCommand.php
+++ src/Commands/ResultsCommand.php
@@ -10,20 +10,62 @@ class ResultsCommand extends \WP_CLI_Command
     }
 
     /**
-     * command usage: wp results results_tab
+     * command usage: wp results results_default
      */
-    public function results_tab()
+    public function results_default()
     {
-        $views_plugin_dir = RESULTS_PLUGIN_DIR . 'templates/ResultsTab/Views/results.blade.php';
+        $views_plugin_dir = RESULTS_PLUGIN_DIR . 'templates/ResultsDefault/Views/results.blade.php';
         $views_theme_dir = get_stylesheet_directory() . '/resources/views/partials/builder/results.blade.php';
 
-        $controller_plugin_dir = RESULTS_PLUGIN_DIR . 'templates/ResultsTab/Controller/Results.php';
+        $controller_plugin_dir = RESULTS_PLUGIN_DIR . 'templates/ResultsDefault/Controller/Results.php';
         $controller_theme_dir = get_stylesheet_directory() . '/app/View/Composers/Results.php';
 
-        $field_plugin_dir = RESULTS_PLUGIN_DIR . 'templates/ResultsTab/Fields/Results.php';
+        $field_plugin_dir = RESULTS_PLUGIN_DIR . 'templates/ResultsDefault/Fields/Results.php';
         $field_theme_dir = get_stylesheet_directory() . '/app/Fields/Results.php';
 
-        $field_partial_plugin_dir = RESULTS_PLUGIN_DIR . 'templates/ResultsTab/Fields/Partials/Results.php';
+        $field_partial_plugin_dir = RESULTS_PLUGIN_DIR . 'templates/ResultsDefault/Fields/Partials/Results.php';
+        $field_partial_theme_dir = get_stylesheet_directory() . '/app/Fields/Partials/Builder/Layouts/Results.php';
+
+        if(!copy($views_plugin_dir, $views_theme_dir)) {
+            echo "\nfailed to copy $views_plugin_dir to $views_theme_dir...\n";
+        }else {
+            echo "\nSuccessfully copy $views_plugin_dir to $views_theme_dir...\n";
+        }
+
+        if(!copy($controller_plugin_dir, $controller_theme_dir)) {
+            echo "\nfailed to copy $controller_plugin_dir to $controller_theme_dir...\n";
+        }else {
+            echo "\nSuccessfully copy $controller_plugin_dir to $controller_theme_dir...\n";
+        }
+
+        if(!copy($field_plugin_dir, $field_theme_dir)) {
+            echo "\nfailed to copy $field_plugin_dir to $field_theme_dir...\n";
+        }else {
+            echo "\nSuccessfully copy $field_plugin_dir to $field_theme_dir...\n";
+        }
+
+        if(!copy($field_partial_plugin_dir, $field_partial_theme_dir)) {
+            echo "\nfailed to copy $field_partial_plugin_dir to $field_partial_theme_dir...\n";
+        }else {
+            echo "\nSuccessfully copy $field_partial_plugin_dir to $field_partial_theme_dir...\n";
+        }
+    }
+
+    /**
+     * command usage: wp results results_with_type
+     */
+    public function results_with_type()
+    {
+        $views_plugin_dir = RESULTS_PLUGIN_DIR . 'templates/ResultsWithType/Views/results.blade.php';
+        $views_theme_dir = get_stylesheet_directory() . '/resources/views/partials/builder/results.blade.php';
+
+        $controller_plugin_dir = RESULTS_PLUGIN_DIR . 'templates/ResultsWithType/Controller/Results.php';
+        $controller_theme_dir = get_stylesheet_directory() . '/app/View/Composers/Results.php';
+
+        $field_plugin_dir = RESULTS_PLUGIN_DIR . 'templates/ResultsWithType/Fields/Results.php';
+        $field_theme_dir = get_stylesheet_directory() . '/app/Fields/Results.php';
+
+        $field_partial_plugin_dir = RESULTS_PLUGIN_DIR . 'templates/ResultsWithType/Fields/Partials/Results.php';
         $field_partial_theme_dir = get_stylesheet_directory() . '/app/Fields/Partials/Builder/Layouts/Results.php';
 
         if(!copy($views_plugin_dir, $views_theme_dir)) {
---
+++ templates/ResultsDefault/Fields/Results.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Fields;
+
+use Log1x\AcfComposer\Field;
+use StoutLogic\AcfBuilder\FieldsBuilder;
+
+class Results extends Field
+{
+    /**
+     * The field group.
+     *
+     * @return array
+     */
+    public function fields()
+    {
+        $Results = new FieldsBuilder('results', [
+            'title' => 'Fields',
+            'menu_order' => 0,
+            'position' => 'acf_after_title',
+            'style' => 'default',
+            'label_placement' => 'top',
+            'instruction_placement' => 'label',
+            'active' => true,
+            'show_in_rest' => 0,
+        ]);
+
+        $Results
+            ->setLocation('post_type', '==', 'results');
+
+        $Results
+            ->addTaxonomy('year', [
+                'label' => 'Year',
+                'taxonomy' => 'result_years',
+                'add_term' => 0,
+                'save_terms' => 1,
+                'load_terms' => 1,
+                'return_format' => 'object',
+                'field_type' => 'select',
+            ])
+            ->addDatePicker('date', [
+                'label' => 'Date',
+                'required' => 1,
+                'display_format' => 'd/m/Y',
+                'return_format' => 'j M Y',
+                'first_day' => 1,
+            ])
+            ->addFile('press_release', [
+                'label' => 'Press Release',
+                'instructions' => 'Please upload or select a press release that is in the format of PDF.',
+                'wrapper' => array(
+                    'width' => '50',
+                ),
+                'mime_types' => 'pdf',
+            ])
+            ->addFile('report', [
+                'label' => 'Report',
+                'instructions' => 'Please upload or select a report that is in the format of PDF.',
+                'mime_types' => 'pdf',
+            ])
+            ->addFile('presentation', [
+                'label' => 'Presentation',
+                'instructions' => 'Please upload or select a presentation that is in the format of PDF.',
+                'mime_types' => 'pdf',
+            ]);
+
+        return $Results->build();
+    }
+}
